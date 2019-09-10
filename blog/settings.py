adcee26bb9417bc7ea72ce0fd31eac6b298c240e
Lowe54 <jameslowe241@gmail.com>
2019-09-10 21:42:47 +0000

    Allowed hosts updated

--- blog/settings.py
+++ blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://lowe541-django-blog-test.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'lowe541-django-blog-test.herokuapp.com']
 
 
 # Application definition
